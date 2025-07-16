614688e90642c6e40a434d342f89f6d0dffcd3f2
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-16 10:47:21 +0530

    add authentication & herosection

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "@/components/HeroSection";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <div className="grid-background"></div>
+
+      <HeroSection />
+
+    </div>
+
+  );
+}
