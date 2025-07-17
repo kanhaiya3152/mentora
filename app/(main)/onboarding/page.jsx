0cd4ccff7c7dfe1a369baf84dc406908d45db5e3
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-17 23:32:33 +0530

    add DashboardView and OnboardingForm components; implement insights display and user onboarding logic

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,8 +1,15 @@
 import { industries } from "@/data/industries";
 import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 
 export default async function OnboardingPage() {
   // Check if user is already onboarded
+  const {isOnboarded} = await getUserOnboardingStatus();
+
+  if(isOnboarded) {
+    redirect("/dashboard")
+  }
 
   return (
     <main>
