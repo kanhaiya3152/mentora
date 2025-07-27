dd0a882609ed746195691c15153d7723aeb55cdc
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-27 22:04:43 +0530

    update image pathname

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     images:{
         remotePatterns:[{
             protocol:"https",
-            hostname:"randomuser.me"
+            hostname:"randomuser.me",
+            pathname: '/api/portraits/**',
         }]
     }
 };
