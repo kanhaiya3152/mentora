72df845af4c33de536f491fa4b69d135ec52a0aa
Kanhaiya Mandal <kanhaiya3152@gmail.com>
2025-07-27 22:37:43 +0530

    final commit

--- app/page.jsx
+++ app/page.jsx
@@ -113,6 +113,9 @@ export default function Home() {
                           src={testimonial.image}
                           alt={testimonial.author}
                           className="rounded-full object-cover border-2 border-primary/20"
+                          onError={(e) => {
+                            e.currentTarget.src = "/images/default-avatar.png"; // local fallback from public folder
+                          }}
                         />
                       </div>
                       <div>
